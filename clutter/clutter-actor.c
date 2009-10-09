a1e1e8d10681e0cb64bba8c4b8de36be8a1901dc
Damien Lespiau <damien.lespiau@intel.com>
2009-10-09 17:18:34 +0100

    [actor] Unset the IN_DESTRUCTION flag when leaving ::destroy()
    The IN_DESTRUCTION flag is set around the unrealization and disposal of
    the actor in clutter_actor_destroy() but is never unset (it's set twice
    instead).
    
    Reviewed-by: Emmanuele Bassi <ebassi@linux.intel.com>

--- clutter/clutter-actor.c
+++ clutter/clutter-actor.c
@@ -4328,7 +4328,7 @@ clutter_actor_destroy (ClutterActor *self)
 
       g_object_run_dispose (G_OBJECT (self));
 
-      CLUTTER_SET_PRIVATE_FLAGS (self, CLUTTER_ACTOR_IN_DESTRUCTION);
+      CLUTTER_UNSET_PRIVATE_FLAGS (self, CLUTTER_ACTOR_IN_DESTRUCTION);
     }
 
   g_object_unref (self);
