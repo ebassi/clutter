6309c5daa8266babfe8a94188540c0eabab7c2b9
Neil Roberts <neil@linux.intel.com>
2010-07-14 19:45:06 +0100

    Fix test-shader for GLES2
    The tests can't rely on using the internal HAVE_* macros that are
    generated by configure.ac. It's better to use the public COGL_HAS_*
    macros from cogl-defines.h This was causing it to use the GL versions
    of the variable names.

--- tests/interactive/test-shader.c
+++ tests/interactive/test-shader.c
@@ -20,7 +20,7 @@ typedef struct
 
 /* These variables are used instead of the standard GLSL variables on
    GLES 2 */
-#ifdef HAVE_COGL_GLES2
+#ifdef COGL_HAS_GLES
 
 #define GLES2_VARS \
   "precision mediump float;\n" \
@@ -29,13 +29,13 @@ typedef struct
 #define TEX_COORD "tex_coord[0]"
 #define COLOR_VAR "frag_color"
 
-#else /* HAVE_COGL_GLES2 */
+#else /* COGL_HAS_GLES */
 
 #define GLES2_VARS ""
 #define TEX_COORD "gl_TexCoord[0]"
 #define COLOR_VAR "gl_Color"
 
-#endif /* HAVE_COGL_GLES2 */
+#endif /* COGL_HAS_GLES */
 
 /* a couple of boilerplate defines that are common amongst all the
  * sample shaders
@@ -303,7 +303,7 @@ button_release_cb (ClutterActor    *actor,
   return FALSE;
 }
 
-#ifdef HAVE_COGL_GLES2
+#ifdef COGL_HAS_GLES
 static gboolean
 timeout_cb (gpointer data)
 {
@@ -316,7 +316,7 @@ timeout_cb (gpointer data)
 
   return TRUE;
 }
-#endif /* HAVE_COGL_GLES2 */
+#endif /* COGL_HAS_GLES */
 
 G_MODULE_EXPORT gint
 test_shader_main (gint argc, gchar *argv[])
@@ -416,7 +416,7 @@ test_shader_main (gint argc, gchar *argv[])
   g_signal_connect (actor, "button-release-event",
                     G_CALLBACK (button_release_cb), NULL);
 
-#ifdef HAVE_COGL_GLES2
+#ifdef COGL_HAS_GLES
   /* On an embedded platform it is difficult to right click so we will
      cycle through the shaders automatically */
   g_timeout_add_seconds (3, timeout_cb, actor);
