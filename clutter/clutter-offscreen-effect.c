2e30c29c5cefc7c765da36d1c8d3a5d4d235b613
Neil Roberts <neil@linux.intel.com>
2010-11-25 10:56:50 +0000

    clutter-offscreen-effect: Don't recreate the material when FBO changes
    Previously whenever the size of the FBO changes it would create a new
    material and attach the texture to it. This is not good for Cogl
    because it throws away any cached state for the material. In
    test-rotate the size of the FBO changes constantly so it effectively
    uses a new material every paint. For shader effects this also ends up
    relinking the shader every paint because the linked programs are part
    of the material state.
    (cherry picked from commit 6ddb8397110f6e6998e0262e5d3dd604cd93d670)
    
    Signed-off-by: Emmanuele Bassi <ebassi@linux.intel.com>

--- clutter/clutter-offscreen-effect.c
+++ clutter/clutter-offscreen-effect.c
@@ -153,13 +153,8 @@ update_fbo (ClutterEffect *effect)
       priv->offscreen != COGL_INVALID_HANDLE)
     return TRUE;
 
-  if (priv->target != COGL_INVALID_HANDLE)
-    {
-      cogl_handle_unref (priv->target);
-      cogl_handle_unref (priv->offscreen);
-    }
-
-  priv->target = cogl_material_new ();
+  if (priv->target == COGL_INVALID_HANDLE)
+    priv->target = cogl_material_new ();
 
   texture = clutter_offscreen_effect_create_texture (self, width, height);
   if (texture == COGL_INVALID_HANDLE)
@@ -174,6 +169,9 @@ update_fbo (ClutterEffect *effect)
    */
   priv->target_width = cogl_texture_get_width (texture);
   priv->target_height = cogl_texture_get_height (texture);
+
+  if (priv->offscreen != COGL_INVALID_HANDLE)
+    cogl_handle_unref (priv->offscreen);
 
   priv->offscreen = cogl_offscreen_new_to_texture (texture);
   if (priv->offscreen == COGL_INVALID_HANDLE)
